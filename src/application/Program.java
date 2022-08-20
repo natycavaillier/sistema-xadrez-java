745171140e9c38f5e2e310086d68f53c441b5c6c
Natalia Cavaillier <nataliaabreu2000@hotmail.com>
2022-08-20 20:21:11 -0300

    First class: Position

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import application.boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+        
+        Position pos = new Position(3, 5);
+
+        System.out.println(pos);
+    }
+}
