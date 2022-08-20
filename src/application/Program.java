fd69c0f60fc78c5e4e0d69e64f08a32ca14a5c7c
Natalia Cavaillier <nataliaabreu2000@hotmail.com>
2022-08-20 20:31:32 -0300

    Começando a implementar Board e Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,11 @@
 package application;
 
-import application.boardgame.Position;
+import application.boardgame.Board;
 
 public class Program {
     public static void main(String[] args) throws Exception {
         
-        Position pos = new Position(3, 5);
+        Board board = new Board(8, 8);
 
-        System.out.println(pos);
     }
 }
---
+++ src/application/boardgame/Piece.java
@@ -0,0 +1,26 @@
+package application.boardgame;
+
+public abstract class Piece {
+
+    protected Position position;
+    private Board board;
+
+    //A posição de uma peça recém criada deve ser inicialmente nula, pois deverá mostrar que essa
+    //peça ainda não foi colocada no tabuleiro
+
+    public Piece(Board board) {
+        this.board = board;
+        //Se não colocasse o valor nulo, ainda assim o java setaria a posição como nula
+        position = null;
+    }
+
+
+    protected Board getBoard() {
+        return board;
+    }
+
+
+    
+    
+    
+}
