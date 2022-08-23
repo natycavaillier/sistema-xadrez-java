591917e8d380e81620b3895eb00873f539e452a9
Natalia Cavaillier <nataliaabreu2000@hotmail.com>
2022-08-23 13:51:44 -0300

    Tratamento melhor para a Promoção

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -129,9 +128,6 @@ public class ChessMatch {
     public ChessPiece replacePromotedPiece(String type) {
         if (promoted == null) {
             throw new IllegalStateException("There is no piece to be promoted");
-        }
-        if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-            throw new InvalidParameterException("Invalid type for promotion");
         }
 
         Position pos = promoted.getChessPosition().toPosition();
